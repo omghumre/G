5afd18cf03eda51874295e96da74f74729e1f3a6
omg12347 <omghumre74@gmail.com>
2024-03-06 09:09:07 +0530

    .

--- slics_loop.go
+++ slics_loop.go
@@ -1,15 +1,28 @@
 package main
 
 import "fmt"
+import "sort"
 
 func main() {
-	numbers := [6]int{1, 2, 3, 4, 5, 6}
+	// numbers := []int{1, 9, 8, 4, 5, 6}
 
 	// for i := 0; i < len(numbers); i++ {
 	// 	fmt.Println(numbers[i])
 	// }
 	// making slice from array
-	intSlice := numbers[2:5]
-	fmt.Println(intSlice)
 
+	// intSlice := numbers[2:5]
+	// fmt.Println(intSlice)
+	
+		// sort.Ints(numbers) //            Ints is int slice, used to sort slice
+		// fmt.Println(numbers)
+
+slices:= []string {"i","the","best","policy"}	
+sort.Strings(slices)
+for _,ele := range slices{
+	fmt.Printf("%s",ele)
+}
+
+status := sort.StringsAreSorted(slices)
+println(status)
 }
